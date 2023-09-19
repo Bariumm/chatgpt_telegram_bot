0574a93dc3e2a5c982cd53c7165895d0850fa2e7
Barium <peter.kuehfuss@gmail.com>
2023-09-19 12:41:03 +0200

    Update config.py to use environment variables

--- bot/config.py
+++ bot/config.py
@@ -1,3 +1,4 @@
+import os
 import yaml
 import dotenv
 from pathlib import Path
@@ -12,16 +13,19 @@ with open(config_dir / "config.yml", 'r') as f:
 config_env = dotenv.dotenv_values(config_dir / "config.env")
 
 # config parameters
-telegram_token = config_yaml["telegram_token"]
-openai_api_key = config_yaml["openai_api_key"]
+telegram_token = os.environ["telegram_token"]
+openai_api_key = os.environ["openai_api_key"]
+allowed_telegram_usernames = os.environ["allowed_telegram_usernames"]
+#telegram_token = config_yaml["telegram_token"]
+#openai_api_key = config_yaml["openai_api_key"]
 openai_api_base = config_yaml.get("openai_api_base", None)
-allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
+#allowed_telegram_usernames = config_yaml["allowed_telegram_usernames"]
 new_dialog_timeout = config_yaml["new_dialog_timeout"]
 enable_message_streaming = config_yaml.get("enable_message_streaming", True)
 return_n_generated_images = config_yaml.get("return_n_generated_images", 1)
 image_size = config_yaml.get("image_size", "512x512")
 n_chat_modes_per_page = config_yaml.get("n_chat_modes_per_page", 5)
-mongodb_uri = f"mongodb://mongo:{config_env['MONGODB_PORT']}"
+mongodb_uri = f"mongodb://{os.environ['mongodb_ip']}:{os.environ['mongodb_port']}"
 
 # chat_modes
 with open(config_dir / "chat_modes.yml", 'r') as f:
